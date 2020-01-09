396e565b82198c2edf96c2ee744898ffb20da44d
Samrat De <rajmalhotra2016iitjee@gmail.com>
2020-01-09 09:49:11 +0530

    Updated Prims algo

--- Prims.cpp
+++ Prims.cpp
@@ -1,23 +1,21 @@
 //	Add vertex to the growing spanning tree
 
-ll prims(int src)				// v -> vec(pi(ll,ll))
-{	min_heap(pi(ll,ll)) q;
-	ll mincost=0;
-	pi(ll,ll) p;
+int prims(int src){				// v -> vec(pi(dist,node))
+	min_heap(pi(int,int)) q;
+	int minCost=0;
+	pi(int,int) p;
 	q.push({0,src});
-	while(!q.empty())
-	{	p=q.front();
+	while(!q.empty()){
+		p=q.top();
 		q.pop();
-		ll x=p.second;
+		int x=p.second;
 		if(vis[x])
 			continue;
-		mincost+=p.first;
+		minCost+=p.first;
 		vis[x]=1;
 		rep(i,0,v[x].size())
 			if(!vis[v[x][i].second])
-			{	q.push(v[x][i]);
-				vis[v[x][i].second]=1;
-			}
+				q.push(v[x][i]);
 	}
-	return mincost;
+	return minCost;
 }
