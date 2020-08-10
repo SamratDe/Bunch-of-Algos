5db6a3db2d08c8f1128d9311c8cd787cda464384
Samrat De <32265336+SamratDe@users.noreply.github.com>
2020-08-11 00:53:15 +0530

    updated bfs code

--- BFS.cpp
+++ BFS.cpp
@@ -1,15 +1,28 @@
-void bfs(int src)
-{	queue<int> q;
+// BFS code
+
+// LOGIC :
+// First move horizontally and visit all the nodes of the current layer
+// Move to the next layer
+
+void bfs(int src, int nodeSize) {
+	queue <int>  q;
+	vector <bool> visited(nodeSize + 1, false);
+
 	q.push(src);
-	vis[src]=1;
-	while(!q.empty())
-	{	int p=q.front();
+	visited[src] = true;
+
+	while (!q.empty()) {
+		int u = q.front();
+		// cout << "Node : "<< u << " ";
 		q.pop();
-		rep(i,0,v[p].size())
-		{	if(!vis[v[p][i]])
-			{	q.push(v[p][i]);
-				vis[v[p][i]]=1;
+		for (int i = 0; i < graph[u].size(); i++) {
+			int v = graph[u][i];
+			if (visited[v] == false) {
+				q.push(v);
+				visited[v] = true;
 			}
 		}
 	}
+
+	// cout << endl;
 }
