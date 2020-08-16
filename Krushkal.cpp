bf4cc4959bc35a90942ae465ee9f6b2ee42c3ec1
Samrat De <32265336+SamratDe@users.noreply.github.com>
2020-08-16 17:43:44 +0530

    updated the style & syntax

--- Krushkal.cpp
+++ Krushkal.cpp
@@ -1,44 +1,64 @@
-int par[100005],rnk[100005];
+// SPANNING TREE : includes every vertex of the parent graph &  is a tree.
+// MINIMUM SPANNING TREE : where the cost is minimum among all the spanning trees
 
-void intialize(int n)
-{	rep(i,0,n)
-	{	par[i]=i;
-		rnk[i]=1;
+// Kruskal code
+
+// LOGIC :
+// builds the spanning tree by ADDING EDGES one by one into a growing spanning tree
+// in each iteration it finds an edge which has least weight (GREEDY)
+// add it to the growing spanning tree. (EDGES WHICH DON'T FORM A CYCLE)
+
+#include <queue>
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+// GRAPH REPRESENTATION : 
+vector <pair <int, pair <int, int> > > edges;
+
+// DSU
+int par[101], sz[101];
+
+void init(){
+	for (int i = 0; i < 101; i++) {
+		par[i] = i;
+		sz[i] = 1;
 	}
 }
 
-int find(int x)
-{	if(par[x]!=x)
-		par[x]=find(par[x]);
+int find(int x) {
+	if (par[x] != x) {
+		par[x] = find(par[x]);
+	}
 	return par[x];
 }
 
-void merge(int x,int y)
-{	int xr=find(x),yr=find(y);
-	if(xr==yr)
+void merge(int x, int y) {
+	int xr = find(x), yr = find(y);
+	if (xr == yr) {
 		return;
-	if(rnk[xr]>rnk[yr])
-		par[yr]=xr;
-	else if(rnk[yr]>rnk[xr])
-		par[xr]=yr;
-	else{
-		par[yr]=xr;
-		rnk[xr]+=1;
+	}
+	if (sz[xr] >= sz[yr]) {
+		par[yr] = xr;
+		sz[xr] += sz[yr];
+	} else {
+		par[xr] = yr;
+		sz[yr] += sz[xr];
 	}
 }
 
-//	Adding edges one by one to the growing spanning tree(greedy approach)
-//	adds the edge which has least weight
-
-ll krushkal(pi(ll,pi(ll,ll)) p[],int edges)	 	//p[] -> {wt, {vertex1, vertex2} } sorted by wt
-{	ll cost,mincost=0;
-	rep(i,0,edges) 
-	{	ll x=p[i].second.first,y=p[i].second.second;
-		cost=p[i].first;
-		if(find(x)!=find(y))
-		{	mincost+=cost;
-			merge(x,y);
+int kruskal(int src) {
+	int ans = 0;
+	sort(edges.begin(), edges.end());
+	for (int i = 0; i < edges.size(); i++) {
+		int u = edges[i].second.first;
+		int v = edges[i].second.first;
+		int val = edges[i].first;
+		if (find(u) != find(v)) {
+			ans += val;
+			merge(u, v);
 		}
 	}
-	return mincost;
+	return ans;
 }
