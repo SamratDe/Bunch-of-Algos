89d6ead29b75df142898dcfb47acc1a73c231934
Samrat De <desamrat31@gmail.com>
2021-05-08 19:26:00 +0530

    updated syntax of DSA(by size) and Bipartite graph checking

---
+++ Bipartite_graph_checking.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+// sample
+const int N = 100001;
+vector<int> color(N, 0);
+vector<int> graph[N];
+vector<bool> vis(N, false);
+
+// using 2 colors dfs code
+bool checkForBipartite(int src) {
+	for(int i = 0; i < graph[src].size(); i++) {
+		int v = graph[src][i];
+		if (!vis[v]) {
+			vis[v] = true;
+			color[v] = !color[src];
+			if (!checkForBipartite(v)) return false;
+		} else if (color[v] == color[src]) {
+			return false;
+		}
+	}
+	return true;
+}
--- DSU_(by_size).cpp
+++ DSU_(by_size).cpp
@@ -1,28 +1,28 @@
-int par[N],sz[N];
+const int N = 100001;
+int par[N], sz[N];
 
-void init(){
-	rep(i,0,N){
-		par[i]=i;
-		sz[i]=1;
+void init() {
+	for (int i = 0; i < N; i++) {
+		par[i] = i;
+		sz[i] = 1;
 	}
 }
 
-int find(int x){
-	if(par[x]!=x)
-		par[x]=find(par[x]);
+int find(int x) {
+	if (par[x] != x) {
+		par[x] = find(par[x]);
+	}
 	return par[x];
 }
 
-void merge(int x,int y){
-	int xr=find(x),yr=find(y);
-	if(xr==yr)
-		return;
-	if(sz[xr]>=sz[yr]){
-		par[yr]=xr;
-		sz[xr]+=sz[yr];
-	}
-	else{
-		par[xr]=yr;
-		sz[yr]+=sz[xr];
+void merge(int x, int y) {
+	int xr = find(x), yr = find(y);
+	if (xr == yr) return;
+	if (sz[xr] >= sz[yr]) {
+		par[yr] = xr;
+		sz[xr] += sz[yr];
+	} else{
+		par[xr] = yr;
+		sz[yr] += sz[xr];
 	}
 }
